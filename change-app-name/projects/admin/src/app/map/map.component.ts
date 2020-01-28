2a882b86c9bc93488a8076f8796fe2d029f4157b
soumya <soumya@SDRC.COM>
2020-01-28 13:21:12 +0530

    code optimization map

--- change-app-name/projects/admin/src/app/map/map.component.ts
+++ change-app-name/projects/admin/src/app/map/map.component.ts
@@ -7,33 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() { }
 
-  // lat = 43.879078;
-  // lng = -103.4615581;
   lat: number = 51.673858;
   lng: number = 7.815982;
-  zoom=4;
+  zoom = 4;
+  mapType;
   selectedMarker;
-  markers: any = [
-    // These are all just random coordinates from https://www.random.org/geographic-coordinates/
-    { lat: 22.33159, lng: 105.63233, alpha: 1 },
-    { lat: 7.92658, lng: -12.05228, alpha: 1 },
-    { lat: 48.75606, lng: -118.859, alpha: 1 },
-    { lat: 5.19334, lng: -67.03352, alpha: 1 },
-    { lat: 12.09407, lng: 26.31618, alpha: 1 },
-    { lat: 47.92393, lng: 78.58339, alpha: 1 }
-  ];
+  readonly mapTypeIds: any = {
+    ROADMAP: "ROADMAP",// (normal, default 2D map)
+    SATELLITE: "SATELLITE", //(photographic map)
+    HYBRID: "HYBRID", //(photographic map + roads and city names)
+    TERRAIN: "TERRAIN" //(map with mountains, rivers, etc.)
+  }
+  markers: Marker[] = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A',
+      draggable: true,
+      isSelected: false
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B',
+      draggable: false,
+      isSelected: false
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: true,
+      isSelected: false
+    }
+  ]
 
-  latitude = -28.68352;
-  longitude = -147.20785;
-  mapType = 'map';
-  addMarker(lat: number, lng: number) {
-    this.markers.push({ lat, lng, alpha: 1 });
+  constructor() {
+    this.defaultInitialization();
   }
 
+  defaultInitialization() {
+    this.mapType = this.mapTypeIds.ROADMAP
+  }
   max(coordType: 'lat' | 'lng'): number {
     return Math.max(...this.markers.map(marker => marker[coordType]));
   }
@@ -42,57 +59,30 @@ export class MapComponent implements OnInit {
     return Math.min(...this.markers.map(marker => marker[coordType]));
   }
 
-  selectMarker(event) {
-    this.selectedMarker = {
-      lat: event.latitude,
-      lng: event.longitude,
-      draggable: true
-    };
+  selectMarker(event, markerItem: any, index: number) {
+    this.markers.forEach(eachMarker => { eachMarker.isSelected = false });
+    markerItem.isSelected = true;
+    this.selectedMarker = markerItem;
   }
 
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
-
-  mapClicked($event: any) {
-    this.markers2.push({
+  addMarker($event: any) {
+    this.markers.push({
       lat: $event.coords.lat,
       lng: $event.coords.lng,
+      isSelected: false,
       draggable: true
     });
   }
 
-  markerDragEnd(m: marker, $event: MouseEvent) {
+  markerDragEnd(m: Marker, $event: MouseEvent) {
     console.log('dragEnd', m, $event);
   }
-
-  markers2: any[] = [
-    {
-      lat: 51.673858,
-      lng: 7.815982,
-      label: 'A',
-      draggable: true
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
-    }
-  ]
-
 }
 
-interface marker {
+interface Marker {
   lat: number;
   lng: number;
   label?: string;
   draggable: boolean;
+  isSelected?: boolean;
 }
